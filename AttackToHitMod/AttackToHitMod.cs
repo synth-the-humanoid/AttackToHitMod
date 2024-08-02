214363677c575263e390260a2b6fa28bfabc3b24
synth <wavydave2000@gmail.com>
2024-08-02 16:44:12 -0400

    Add project files.

---
+++ AttackToHitMod/AttackToHitMod.cs
@@ -0,0 +1,14 @@
+﻿using KHMI;
+using KHMI.Types;
+
+namespace AttackToHitMod
+{
+    public class AttackToHitMod : KHMod
+    {
+        public AttackToHitMod(ModInterface mi) : base(mi)
+        {
+            KHCommand attackCommand = KHCommand.FromID(mi.dataInterface, 1);
+            attackCommand.ActionID = 0x0156;
+        }
+    }
+}
